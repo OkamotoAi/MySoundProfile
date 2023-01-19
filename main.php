f0b43b29fc6a1ef27fce14360e6de41f53508408
OkamotoAi <okamotoai0217@icloud.com>
2023-01-19 09:53:53 +0900

    add file

---
+++ main.php
@@ -0,0 +1,25 @@
+require 'vendor/autoload.php';
+
+$session = new SpotifyWebAPI\Session(
+    'CLIENT_ID',
+    'CLIENT_SECRET',
+    'REDIRECT_URI'
+);
+
+$api = new SpotifyWebAPI\SpotifyWebAPI();
+
+if (isset($_GET['code'])) {
+    $session->requestAccessToken($_GET['code']);
+    $api->setAccessToken($session->getAccessToken());
+
+    print_r($api->me());
+} else {
+    $options = [
+        'scope' => [
+            'user-read-email',
+        ],
+    ];
+
+    header('Location: ' . $session->getAuthorizeUrl($options));
+    die();
+}

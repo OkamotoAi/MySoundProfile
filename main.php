a5f56dbdd5b0861f232c3d085dc987f053b5316f
OkamotoAi <okamotoai0217@icloud.com>
2023-01-21 21:04:08 +0900

    add spotifyAPI

--- main.php
+++ main.php
@@ -1,25 +1,31 @@
-require 'vendor/autoload.php';
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
 $session = new SpotifyWebAPI\Session(
-    'CLIENT_ID',
-    'CLIENT_SECRET',
-    'REDIRECT_URI'
+    $_ENV['SPOTIFY_CLIENT_ID'],
+    $_ENV['SPOTIFY_CLIENT_SECRET'],
+    $_ENV['REDIRECT_URI']
 );
 
 $api = new SpotifyWebAPI\SpotifyWebAPI();
+
 
 if (isset($_GET['code'])) {
     $session->requestAccessToken($_GET['code']);
     $api->setAccessToken($session->getAccessToken());
 
-    print_r($api->me());
 } else {
-    $options = [
-        'scope' => [
-            'user-read-email',
-        ],
-    ];
-
-    header('Location: ' . $session->getAuthorizeUrl($options));
+    header('Location: ' . $session->getAuthorizeUrl(array(
+        'scope' => array(
+            'playlist-read-private', 
+            'playlist-read-collaborative'
+        )
+    )));
     die();
 }
+
+print_r($api->getTrack('7EjyzZcbLxW7PaaLua9Ksb'));
+
+?>
